13af643b53683ea5ac090dd0d0fb1f4f141dfd0b
Romaric Juniet <romaric.juniet@gmail.com>
2019-05-04 17:50:03 +0800

    years

--- loader.js
+++ loader.js
@@ -3,13 +3,13 @@ const fs = require("fs");
 
 // https://www.flickr.com/services/api/flickr.photos.search.htm
 // https://www.flickr.com/services/api/explore/flickr.photos.search
-async function loadPhotos(page = 0) {
+async function loadPhotos(page = 0, minTakenDate, maxTakenDate) {
   const params = Object.entries({
     api_key: "407902b4e99781ae7d33552db35a119c",
     bbox: "103.63123,1.180947,104.040871,1.458106",
     extras: "date_taken,geo,url_s,views",
-    min_taken_date: new Date(2018, 0, 1).getTime() / 1000,
-    max_taken_date: new Date(2019, 0, 1).getTime() / 1000,
+    min_taken_date: minTakenDate,
+    max_taken_date: maxTakenDate,
     per_page: "500",
     sort: "interestingness-desc"
   })
@@ -24,19 +24,20 @@ async function loadPhotos(page = 0) {
   };
 }
 
-(async function() {
-  const { photos, pages } = await loadPhotos(0);
-  const pagesToLoad = Math.min(pages, 30);
+async function getAllPhotosBetween(minTakenDate, maxTakenDate) {
+  const { photos, pages } = await loadPhotos(0, minTakenDate, maxTakenDate);
+  const pagesToLoad = Math.min(pages, 10);
   const promises = [];
 
-  console.log(`loading ${pagesToLoad} pages`);
   for (let i = 1; i < pagesToLoad; i++) {
-    promises.push(loadPhotos(i).then(x => x.photos));
+    promises.push(
+      loadPhotos(i, minTakenDate, maxTakenDate).then(x => x.photos)
+    );
   }
-  const allPhotos = photos.concat(...(await Promise.all(promises)));
+  return photos.concat(...(await Promise.all(promises)));
+}
 
-  console.log(`${allPhotos.length} photos`);
-
+async function saveGeoJson(allPhotos, filename) {
   var geojsonFeatures = allPhotos.map(photo => ({
     type: "Feature",
     properties: {
@@ -55,5 +56,27 @@ async function loadPhotos(page = 0) {
     type: "FeatureCollection",
     features: geojsonFeatures
   });
-  fs.writeFileSync("2018.geojson", geojson);
+  fs.writeFileSync("data/" + filename + ".geojson", geojson);
+}
+
+async function generateFor(year, year2) {
+  const minTakenDate = new Date(year, 0, 1).getTime() / 1000;
+  const maxTakenDate = new Date(year2 || year, 11, 31).getTime() / 1000;
+
+  const allPhotos = await getAllPhotosBetween(minTakenDate, maxTakenDate);
+
+  console.log(`year ${year} ${allPhotos.length} photos`);
+
+  saveGeoJson(allPhotos, year);
+}
+
+(async function() {
+  generateFor(1950, 1959);
+  generateFor(1960, 1969);
+  generateFor(1970, 1979);
+  generateFor(1980, 1989);
+  generateFor(1990, 1999);
+  for (let year = 2000; year < 2020; year++) {
+    generateFor(year);
+  }
 })();
